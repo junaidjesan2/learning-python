9080a5f4638142088091f556b4a0ab66b9b281b2
junaidjesan2 <junaidjesan2@gmail.com>
2024-03-14 00:12:00 +0600

    into this ripo i will practice and will implement all learning concepts

---
+++ hello.py
@@ -0,0 +1,11 @@
+list=["apple","banana","orange","mango"]
+
+print(list)
+for i in list :
+    print(i)
+
+
+first_tump=("junaid","ahamed","jesan")
+
+for i in range(len(first_tump)):
+    print(first_tump[i])
