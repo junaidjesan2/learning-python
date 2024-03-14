ac0c9c66e19ba67b0fd4a0b3cb38eb179a288500
junaidjesan2 <junaidjesan2@gmail.com>
2024-03-14 06:06:28 +0600

    set in python is semilar to js object but it's functionality isn't. to access any data from set it will return a boolean value insted of the value

--- hello.py
+++ hello.py
@@ -11,8 +11,14 @@
 #     print(first_tump[i])
 
     # join tumple
-join1=(1,2,3,4,5)
-join2=(6,7,8)
-join=join1+join2
-print(join)
+# join1=(1,2,3,4,5)
+# join2=(6,7,8)
+# join=join1+join2
+# print(join)
 
+set={"orange","banana","dragon"}
+
+for s in set:
+    print(s)
+
+print("fokir" in set)
